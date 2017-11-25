cf017bf2be1f87e83b0eebab190bd024e8e15837
EHenry92 <EHenry692@hotmail.com>
2017-11-25 10:06:34 -0500

    fixed seed file

--- script/oseed.js
+++ script/oseed.js
@@ -23,150 +23,6 @@ const satList = [
   {word: 'blandishment', definition: 'flattery intended to persuade', sentence: 'Despite his nervousness at meeting his in-laws, Dylan was able to offer just the right blandishment to get their approval.'},
   {word: 'bilk', definition: 'cheat somebody out of what is due, especially money', sentence: 'She hoped it was clear that this charity was not just another way to bilk citizens out of what little funds they had.'},
   {word: 'bombastic', definition: 'ostentatiously lofty in style', sentence: 'The cheerleading squad led the crowd in bombastic cheers. '},
-  {word:  'execrable', definition: 'unequivocally detestable'},
-  {word:  'exigent', definition: 'demanding immediate attention'},
-  {word:  'expedient', definition: 'appropriate to a purpose'},
-  {word:  'expiate', definition: 'make amends for'},
-  {word:  'expunge', definition: 'remove by erasing or crossing out or as if by drawing a line'},
-  {word:  'extraneous', definition: 'not belonging to that in which it is contained'},
-  {word:  'extol', definition: 'praise, glorify, or honor'},
-  {word:  'extant', definition: 'still in existence; not extinct or destroyed or lost'},
-  {word:  'expurgate', definition: 'edit by omitting or modifying parts considered indelicate'},
-  {word:  'fallacious', definition: 'containing or based on incorrect reasoning'},
-  {word:  'fatuous', definition: 'devoid of intelligence'},
-  {word:  'fetter', definition: 'a shackle for the ankles or feet'},
-  {word:  'flagrant', definition: 'conspicuously and outrageously bad or reprehensible'},
-{word: 'elegy', definition: 'a mournful poem; a lament for the dead'},
-  {word:  'elicit', definition: 'call forth, as an emotion, feeling, or response'},
-  {word:  'embezzlement', definition: 'the fraudulent appropriation of funds or property'},
-  {word:  'emend', definition: 'make corrections to'},
-  {word:  'emollient', definition: 'a substance with a soothing effect when applied to the skin'},
-  {word:  'empirical', definition: 'derived from experiment and observation rather than theory'},
-  {word:  'emulate', definition: 'strive to equal or match, especially by imitating'},
-  {word:  'enervate', definition: 'weaken mentally or morally'},
-  {word:  'enfranchise', definition: 'grant freedom to, as from slavery or servitude'},
-  {word:  'engender', definition: 'call forth'},
-  {word:  'ephemeral', definition: 'anything short-lived, as an insect that lives only for a day'},
-  {word:  'epistolary', definition: 'written in the form of letters or correspondence'},
-  {word:  'equanimity', definition: 'steadiness of mind under stress'},
-  {word:  'equivocal', definition: 'open to two or more interpretations'},
-  {word:  'espouse', definition: 'choose and follow a theory, idea, policy, etc.'},
-  {word:  'evanescent', definition: 'tending to vanish like vapor'},
-  {word:  'evince', definition: 'give expression to'},
-  {word:  'exacerbate', definition: 'make worse'},
-{word:  'advocate', definition: 'a person who pleads for a person, cause, or idea'},
-  {word:  'affluent', definition: 'having an abundant supply of money or possessions of value'},
-  {word:  'aggrandize', definition: 'embellish; increase the scope, power, or importance of'},
-  {word:  'alacrity', definition: 'liveliness and eagerness'},
-  {word:  'alias', definition: 'a name that has been assumed temporarily'},
-  {word:  'ambivalent', definition: 'uncertain or unable to decide about what course to follow'},
-  {word:  'amenable', definition: 'disposed or willing to comply'},
-  {word:  'amorphous', definition: 'having no definite form or distinct shape'},
-  {word:  'anachronistic', definition: 'chronologically misplaced'},
-  {word: ' anathema', definition: 'a formal ecclesiastical curse accompanied by excommunication'},
-  {word:  'annex', definition: 'attach to'},
-  {word:  'antediluvian', definition: 'of or relating to the period before the biblical flood'},
-  {word:  'antiseptic', definition: 'thoroughly clean and free of disease-causing organisms'},
-  {word:  'apathetic', definition: 'showing little or no emotion or animation'},
-  {word:  'antithesis', definition: 'exact opposite'},
-  {word:  'apocryphal', definition: 'being of questionable authenticity'},
-  {word:  'approbation', definition: 'official approval'},
-  {word:  'arbitrary', definition: 'based on or subject to individual discretion or preference'},
-  {word:  'arboreal', definition: 'of or relating to or formed by trees'},
-  {word:  'arcane', definition: 'requiring secret or mysterious knowledge'},
-  {word:  'archetypal', definition: 'of an original type after which other things are patterned'},
-  {word:  'arrogate', definition: 'seize and take control without authority'},
-  {word:  'ascetic', definition: 'someone who practices self denial as a spiritual discipline'},
-  {word:  'aspersion', definition: 'a disparaging remark'},
-  {word:  'assiduous', definition: 'marked by care and persistent effort'},
-  {word: 'dour', definition: 'showing a brooding ill humor'},
-  {word: 'duplicity', definition: 'acting in bad faith'},
-  {word:  'atrophy', definition: 'a decrease in size of an organ caused by disease or disuse'},
-  {word:  'callous', definition: 'emotionally hardened'},
-  {word: 'calumny', definition: 'a false accusation of an offense'},
-  {word: 'camaraderie', definition: 'the quality of affording easy familiarity and sociability'},
-  {word: 'candor', definition: 'the quality of being honest and straightforward'},
-  {word: 'capitulate', definition: 'surrender under agreed conditions'},
-  {word: 'carouse', definition: 'engage in boisterous, drunken merrymaking'},
-  {word: 'carp', definition: 'any of various freshwater fish of the family Cyprinidae'},
-  {word: 'caucus', definition: 'meet to select a candidate or promote a policy'},
-  {word: 'cavort', definition: 'play boisterously'},
-  {word: 'circumlocution', definition: 'an indirect way of expressing something'},
-  {word: 'circumscribe', definition: 'draw a geometric figure around another figure'},
-  {word: 'circumvent', definition: 'surround so as to force to give up'},
-  {word: 'clamor', definition: 'utter or proclaim insistently and noisily'},
-  {word: 'cogent', definition: 'powerfully persuasive'},
-  {word: 'cognizant', definition: 'having or showing knowledge or understanding or realization'},
-  {word: 'commensurate', definition: 'corresponding in size or degree or extent'},
-  {word: 'complement', definition: 'something added to embellish or make perfect'},
-  {word: 'compunction', definition: 'a feeling of deep regret, usually for some misdeed'},
-  {word: 'concomitant', definition: 'following or accompanying as a consequence'},
-  {word: 'conduit', definition: 'a passage through which water or electric wires can pass'},
-  {word: 'conflagration', definition: 'a very intense and uncontrolled fire'},
-  {word: 'congruity', definition: 'the quality of agreeing; being suitable and appropriate'},
-  {word: 'connive', definition: 'form intrigues (for) in an underhand manner'},
-  {word: 'consign', definition: 'give over to another for care or safekeeping'},
-  {word: 'constituent', definition: 'one of the individual parts making up a composite entity'},
-  {word: 'construe', definition: 'make sense of; assign a meaning to'},
-  {word: 'contusion', definition: 'an injury in which the skin is not broken'},
-  {word: 'contrite', definition: 'feeling or expressing pain or sorrow for sins or offenses'},
-  {word: 'contentious', definition: 'showing an inclination to disagree'},
-  {word: 'contravene', definition: 'go against, as of rules and laws'},
-  {word: 'corpulence', definition: 'the property of excessive fatness'},
-  {word: 'covet', definition: 'wish, long, or crave for'},
-  {word: 'cupidity', definition: 'extreme greed for material wealth'},
-  {word: 'defunct', definition: 'no longer in force or use; inactive'},
-  {word: 'demagogue', definition: 'a leader who seeks support by appealing to popular passions'},
-  {word: 'denigrate', definition: 'charge falsely or with malicious intent'},
-  {word: 'derivative', definition:' a compound obtained from another compound'},
-  {word: 'despot', definition: 'a cruel and oppressive dictator'},
-  {word: 'diaphanous', definition: 'so thin as to transmit light'},
-  {word: 'didactic', definition: 'instructive, especially excessively'},
-  {word: 'dirge', definition: 'a song or hymn of mourning as a memorial to a dead person'},
-  {word: 'debauch', definition: 'a wild gathering involving excessive drinking'},
-  {word: 'debunk', definition: 'expose while ridiculing'},
-  {word: 'disaffected', definition: 'discontented as toward authority'},
-  {word: 'discomfit', definition: "cause to lose one's composure"},
-  {word: 'disparate', definition: 'fundamentally different or distinct in quality or kind'},
-  {word: 'dispel', definition: 'to cause to separate and go in different directions'},
-  {word: 'disrepute', definition: 'the state of being held in low esteem'},
-
-
-  {word: 'cajole', definition: 'influence or urge by gentle urging, caressing, or flattering', sentence: ""},
-  {word: 'cleave', definition: 'separate or cut with a tool, such as a sharp instrument'},
-  {word: 'cobbler', definition: 'a person who makes or repairs shoes'},
-  {word: 'convivial', definition: 'occupied with or fond of the pleasures of good company'},
-  {word: 'dearth', definition: 'an insufficient quantity or number'},
-  {word: 'debacle', definition: 'a sudden and violent collapse'},
-
-  {word: 'divisive', definition: 'dissenting with the majority opinion'},
-  {word: 'dogmatic', definition: 'pertaining to a code of beliefs accepted as authoritative'},
-
-  {word: 'duress', definition: 'compulsory force or threat'},
-  {word: 'eclectic', definition: 'selecting what seems best of various styles or ideas'},
-  {word: 'edict', definition: 'a formal or authoritative proclamation'},
-  {word: 'ebullient', definition: 'joyously unrestrained'},
-  {word: 'egregious', definition: 'conspicuously and outrageously bad or reprehensible'},
-
-  {word:  'exhort', definition: 'spur on or encourage especially by cheers and shouts'},
-
-  {word:  'foil', definition: 'hinder or prevent, as an effort, plan, or desire'},
-  {word:  'forbearance', definition: 'good-natured tolerance of delay or incompetence'},
-  {word:  'fortuitous', definition: 'occurring by happy chance'},
-  {word:  'fractious', definition: 'easily irritated or annoyed'},
-  {word:  'garrulous', definition: 'full of trivial conversation'},
-  {word:  'gourmand', definition: 'a person who is devoted to eating and drinking to excess'},
-  {word:  'grandiloquent', definition: 'lofty in style'},
-  {word:  'gratuitous', definition: 'unnecessary and unwarranted'},
-  {word:  'hapless', definition: 'unfortunate and deserving pity'},
-  {word:  'hegemony', definition: 'the dominance or leadership of one social group over others'},
-  {word:  'heterogenous', definition: 'consisting of elements that are not of the same kind'},
-  {word:  'iconoclast', definition: 'someone who attacks cherished ideas or institutions'},
-  {word:  'idiosyncratic', definition: 'peculiar to the individual'},
-  {word:  'impecunious', definition: 'not having enough money to pay for necessities'},
-  {word:  'impetuous', definition: 'characterized by undue haste and lack of thought'},
-  {word:  'impinge', definition: 'infringe upon'},
-  {word:  'impute', definition: 'attribute or credit to'}
 ];
 const sensoryWords = [
   {word: 'damp', definition: 'slightly wet',image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwjSxcOPibrXAhUc0IMKHW1LBSQQjRwIBw&url=http%3A%2F%2Fwww.envirovent.com%2Fblog%2Fkeep-condensation-and-damp-out-this-winter%2F&psig=AOvVaw1n5ycDdw5aZCvh9VbALFKL&ust=1510611615451846'},
@@ -197,6 +53,8 @@ const mathList = [
 
 
 
+
+
 async function seed () {
   await db.sync({force: true})
   console.log('db synced!')
@@ -213,9 +71,9 @@ async function seed () {
 
   const dictionaries = await Promise.all([
     Dictionary.create({title: 'Sensory Words', image: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiKqtmXjLrXAhWf0YMKHfSpAFkQjRwIBw&url=https%3A%2F%2Fercare24.com%2Ffive-senses%2F&psig=AOvVaw07I5ff4QpSlCb8Xmf4RIZ2&ust=1510612485883176'})
-    .then(_ => {
+    .then(book => {
       for (var m = 0; m < sensoryWords.length; m++)  {
-        Word.create({
+        book.createWord({
           word: sensoryWords[m].word,
           definition: sensoryWords[m].definition,
           verified: true,
@@ -223,41 +81,11 @@ async function seed () {
         })
       }
     })
-    .then(_ => {
-      for (var j=1; j <= sensoryWords.length; j++) {
-        Group.create({
-          wordId: j, dictionaryId: 1
-        })
-      }
-    })
-    .catch(err => console.log(err))
-    ,
-    Dictionary.create({title: 'Elementry Math', image: 'https://cdn.themeasuredmom.com/wp-content/uploads/2015/02/block-play.jpg'})
-    .then(_=> {
-      for (var i = 0; i < mathList.length; i++){
-        Word.create({
-          word: mathList[i].word,
-          definition: mathList[i].definition,
-          level: 2,
-          image: mathList[i].image,
-          sentence: mathList[i].sentence,
-          verified: true
-        })
-      }
-    })
-    .then(_ => {
-      for (var j = (sensoryWords.length + 1) ; j <= (mathList.length + sensoryWords.length); j++) {
-        Group.create({
-          wordId: j, dictionaryId: 2
-        })
-      }
-    })
-    .catch(err => console.log(err))
-    ,
+    .catch(err => console.log(err)),
     Dictionary.create({title: 'SAT Words', image: 'https://www.usnews.com/dims4/USNEWS/6265f15/2147483647/thumbnail/970x647/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2F96%2F8b%2F55ff0c9b43eb955698a681c6238e%2Fresizes%2F1500%2F160318-sat-stock.jpg'})
-    .then(_ => {
+    .then(dictionary => {
       for (var i = 0; i < satList.length; i++){
-        Word.create({
+        dictionary.createWord({
           word: satList[i].word,
           definition: satList[i].definition,
           level: 5,
@@ -267,10 +95,18 @@ async function seed () {
         })
       }
     })
-    .then(_ => {
-      for (var j = (sensoryWords.length + mathList.length + 1); j <= (satList.length + mathList.length + sensoryWords.length); j++) {
-        Group.create({
-          wordId: j, dictionaryId: 3
+    .catch(err => console.log(err))
+    ,
+    Dictionary.create({title: 'Elementry Math', image: 'https://cdn.themeasuredmom.com/wp-content/uploads/2015/02/block-play.jpg'})
+    .then(dictionary => {
+      for (var i = 0; i < mathList.length; i++){
+        dictionary.createWord({
+          word: mathList[i].word,
+          definition: mathList[i].definition,
+          level: 2,
+          image: mathList[i].image,
+          sentence: mathList[i].sentence,
+          verified: true
         })
       }
     })
